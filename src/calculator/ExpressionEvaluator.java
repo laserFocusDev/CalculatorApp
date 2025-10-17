b05a48430813b6957a1181681775f46d7a9dc9f2
Sara Sapre <sarasapre57@gmail.com>
2025-10-17 09:07:29 +0530

    Implement ExpressionEvaluator for mathematical expressions

---
+++ src/calculator/ExpressionEvaluator.java
@@ -0,0 +1,70 @@
+package calculator;
+
+import java.util.Stack;
+
+public class ExpressionEvaluator {
+
+    public static double evaluate(String expr) {
+        // Remove spaces
+        expr = expr.replaceAll("\\s+", "");
+
+        Stack<Double> values = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+
+        for (int i = 0; i < expr.length(); i++) {
+            char c = expr.charAt(i);
+
+            if (Character.isDigit(c) || c == '.') {
+                StringBuilder sb = new StringBuilder();
+                while (i < expr.length() && (Character.isDigit(expr.charAt(i)) || expr.charAt(i) == '.')) {
+                    sb.append(expr.charAt(i));
+                    i++;
+                }
+                i--;
+                values.push(Double.parseDouble(sb.toString()));
+            } else if (c == '(') {
+                ops.push(c);
+            } else if (c == ')') {
+                while (ops.peek() != '(') {
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                }
+                ops.pop();
+            } else if (isOperator(c)) {
+                while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(c)) {
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                }
+                ops.push(c);
+            }
+        }
+
+        while (!ops.isEmpty()) {
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+        }
+
+        return values.pop();
+    }
+
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    private static int precedence(char op) {
+        switch (op) {
+            case '+': case '-': return 1;
+            case '*': case '/': return 2;
+            default: return 0;
+        }
+    }
+
+    private static double applyOp(char op, double b, double a) {
+        switch (op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/':
+                if (b == 0) throw new ArithmeticException("Division by zero");
+                return a / b;
+        }
+        return 0;
+    }
+}
